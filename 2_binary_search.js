cb3d08cd1aac01abf250bcbf64b5cd21be109b0f
Sergei Semenov <dp120291ssv@gmail.com>
2023-12-01 22:14:45 +0100

    linear search

---
+++ 2_binary_search.js
@@ -0,0 +1,10 @@
+const randomArr = [1, 2, 5, 7, 9, 10, 12, 11, 3, 4, 6, 8];
+let iterationCount = 0;
+
+function binarySearch(arr, target) {
+    let startIndex = 0;
+    let endIndex = arr.length - 1;
+}
+
+binarySearch(randomArr, 4);
+console.log('iterationCount', iterationCount);
