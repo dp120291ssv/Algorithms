c55256eaae832f91708429b38c09bb47fee72c3f
Sergei Semenov <dp120291ssv@gmail.com>
2023-12-04 22:02:51 +0100

    binary search

--- 2_binary_search.js
+++ 2_binary_search.js
@@ -1,10 +1,24 @@
-const randomArr = [1, 2, 5, 7, 9, 10, 12, 11, 3, 4, 6, 8];
+const randomArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 let iterationCount = 0;
 
 function binarySearch(arr, target) {
-    let startIndex = 0;
-    let endIndex = arr.length - 1;
+    let start = 0;
+    let end = arr.length;
+    let middle;
+    let isFound = false;
+    let position;
+    while (isFound === false && start <= end) {
+        middle = Math.floor((start + end) / 2);
+        iterationCount += 1;
+        if (arr[middle] === target) {
+            isFound = true;
+            position = middle;
+            return position;
+        }
+        if (target < arr[middle]) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+    }
 }
-
-binarySearch(randomArr, 4);
-console.log('iterationCount', iterationCount);
